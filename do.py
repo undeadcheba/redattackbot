e2e827bb51904e203bddef4a4377b5cc8d1b60c4
Oleh Kostrubin <okostrubin@gmail.com>
2021-01-13 00:18:49 +0100

    getting started

--- do.py
+++ do.py
@@ -34,7 +34,7 @@ class BotHandler:
         return last_update
 
 greet_bot = BotHandler(token)  
-greetings = ('здравствуй', 'привет', 'ку', 'здорово')  
+greetings = ('hi')  
 now = datetime.datetime.now()
 
 def main():  
@@ -53,15 +53,15 @@ def main():
         last_chat_name = last_update['message']['chat']['first_name']
 
         if last_chat_text.lower() in greetings and today == now.day and 6 <= hour < 12:
-            greet_bot.send_message(last_chat_id, 'Доброе утро, {}'.format(last_chat_name))
+            greet_bot.send_message(last_chat_id, 'Hi, {}'.format(last_chat_name))
             today += 1
 
         elif last_chat_text.lower() in greetings and today == now.day and 12 <= hour < 17:
-            greet_bot.send_message(last_chat_id, 'Добрый день, {}'.format(last_chat_name))
+            greet_bot.send_message(last_chat_id, 'Hi, {}'.format(last_chat_name))
             today += 1
 
         elif last_chat_text.lower() in greetings and today == now.day and 17 <= hour < 23:
-            greet_bot.send_message(last_chat_id, 'Добрый вечер, {}'.format(last_chat_name))
+            greet_bot.send_message(last_chat_id, 'Hi, {}'.format(last_chat_name))
             today += 1
 
         new_offset = last_update_id + 1
