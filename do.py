972bb2c3cddd22d8ee7ff8dca3cc065b0d55a465
Oleh Kostrubin <okostrubin@gmail.com>
2021-01-13 00:15:23 +0100

    getting started

--- do.py
+++ do.py
@@ -1,53 +1,73 @@
-import logging
-import os
-import random
-import sys
+import requests
+import datetime
 
-from telegram.ext import Updater, CommandHandler
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+url = "https://api.telegram.org/bot" + BOT_TOKEN + "/"
 
-# Enabling logging
-logging.basicConfig(level=logging.INFO,
-                    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-logger = logging.getLogger()
+class BotHandler:
 
-# Getting mode, so we could define run function for local and Heroku setup
-mode = os.getenv("MODE")
-TOKEN = os.getenv("TOKEN")
-if mode == "dev":
-    def run(updater):
-        updater.start_polling()
-elif mode == "prod":
-    def run(updater):
-        PORT = int(os.environ.get("PORT", "8443"))
-        HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME")
-        # Code from https://github.com/python-telegram-bot/python-telegram-bot/wiki/Webhooks#heroku
-        updater.start_webhook(listen="0.0.0.0",
-                              port=PORT,
-                              url_path=TOKEN)
-        updater.bot.set_webhook("https://{}.herokuapp.com/{}".format(HEROKU_APP_NAME, TOKEN))
-else:
-    logger.error("No MODE specified!")
-    sys.exit(1)
+    def __init__(self, token):
+        self.token = token
+        self.api_url = "https://api.telegram.org/bot{}/".format(token)
 
+    def get_updates(self, offset=None, timeout=30):
+        method = 'getUpdates'
+        params = {'timeout': timeout, 'offset': offset}
+        resp = requests.get(self.api_url + method, params)
+        result_json = resp.json()['result']
+        return result_json
 
-def start_handler(bot, update):
-    # Creating a handler-function for /start command 
-    logger.info("User {} started bot".format(update.effective_user["id"]))
-    update.message.reply_text("Hello from Python!\nPress /random to get random number")
+    def send_message(self, chat_id, text):
+        params = {'chat_id': chat_id, 'text': text}
+        method = 'sendMessage'
+        resp = requests.post(self.api_url + method, params)
+        return resp
 
+    def get_last_update(self):
+        get_result = self.get_updates()
 
-def random_handler(bot, update):
-    # Creating a handler-function for /random command
-    number = random.randint(0, 10)
-    logger.info("User {} randomed number {}".format(update.effective_user["id"], number))
-    update.message.reply_text("Random number: {}".format(number))
+        if len(get_result) > 0:
+            last_update = get_result[-1]
+        else:
+            last_update = get_result[len(get_result)]
 
+        return last_update
 
-if __name__ == '__main__':
-    logger.info("Starting bot")
-    updater = Updater(TOKEN)
+greet_bot = BotHandler(token)  
+greetings = ('здравствуй', 'привет', 'ку', 'здорово')  
+now = datetime.datetime.now()
 
-    updater.dispatcher.add_handler(CommandHandler("start", start_handler))
-    updater.dispatcher.add_handler(CommandHandler("random", random_handler))
+def main():  
+    new_offset = None
+    today = now.day
+    hour = now.hour
 
-    run(updater)
+    while True:
+        greet_bot.get_updates(new_offset)
+
+        last_update = greet_bot.get_last_update()
+
+        last_update_id = last_update['update_id']
+        last_chat_text = last_update['message']['text']
+        last_chat_id = last_update['message']['chat']['id']
+        last_chat_name = last_update['message']['chat']['first_name']
+
+        if last_chat_text.lower() in greetings and today == now.day and 6 <= hour < 12:
+            greet_bot.send_message(last_chat_id, 'Доброе утро, {}'.format(last_chat_name))
+            today += 1
+
+        elif last_chat_text.lower() in greetings and today == now.day and 12 <= hour < 17:
+            greet_bot.send_message(last_chat_id, 'Добрый день, {}'.format(last_chat_name))
+            today += 1
+
+        elif last_chat_text.lower() in greetings and today == now.day and 17 <= hour < 23:
+            greet_bot.send_message(last_chat_id, 'Добрый вечер, {}'.format(last_chat_name))
+            today += 1
+
+        new_offset = last_update_id + 1
+
+if __name__ == '__main__':  
+    try:
+        main()
+    except KeyboardInterrupt:
+        exit()
