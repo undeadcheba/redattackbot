dc5a50d74a62026911a7bd8b6d06e6dafd435e1a
Oleh Kostrubin <okostrubin@gmail.com>
2021-01-13 01:26:47 +0100

    getting started

--- do.py
+++ do.py
@@ -1,71 +1,46 @@
 import requests
 import datetime
 import os
+#greet_bot = BotHandler(os.environ['BOT_TOKEN'])
 
-class BotHandler:
+URL = "https://api.telegram.org/bot%s/" % os.environ['BOT_TOKEN']
+MyURL = "https://redattackbot.herokuapp.com/hook"
 
-    def __init__(self, token):
-        self.token = token
-        self.api_url = "https://api.telegram.org/bot{}/".format(token)
+api = requests.Session()
+application = tornado.web.Application([
+    (r"/", Handler),
+])
 
-    def get_updates(self, offset=None, timeout=30):
-        method = 'getUpdates'
-        params = {'timeout': timeout, 'offset': offset}
-        resp = requests.get(self.api_url + method, params)
-        result_json = resp.json()['result']
-        return result_json
+class Handler(tornado.web.RequestHandler):
+        def post(self):
+            try:
+                logging.debug("Got request: %s" % self.request.body)
+                update = tornado.escape.json_decode(self.request.body)
+                message = update['message']
+                text = message.get('text')
+                if text:
+                    logging.info("MESSAGE\t%s\t%s" % (message['chat']['id'], text))
 
-    def send_message(self, chat_id, text):
-        params = {'chat_id': chat_id, 'text': text}
-        method = 'sendMessage'
-        resp = requests.post(self.api_url + method, params)
-        return resp
+                    if text[0] == '/':
+                        command, *arguments = text.split(" ", 1)
+                        response = CMD.get(command, not_found)(arguments, message)
+                        logging.info("REPLY\t%s\t%s" % (message['chat']['id'], response))
+                        send_reply(response)
+            except Exception as e:
+                logging.warning(str(e))
+                
+         def send_reply(response):
+    		if 'text' in response:
+        		api.post(URL + "sendMessage", data=response)
 
-    def get_last_update(self):
-        get_result = self.get_updates()
-
-        if len(get_result) > 0:
-            last_update = get_result[-1]
-        else:
-            last_update = get_result[len(get_result)]
-
-        return last_update
-
-greet_bot = BotHandler(os.environ['BOT_TOKEN'])
-greetings = ('hi')  
-now = datetime.datetime.now()
-
-def main():  
-    new_offset = None
-    today = now.day
-    hour = now.hour
-
-    while True:
-        #greet_bot.get_updates(new_offset)
-
-        #last_update = greet_bot.get_last_update()
-
-        #last_update_id = last_update['update_id']
-        #last_chat_text = last_update['message']['text']
-        #last_chat_id = last_update['message']['chat']['id']
-        #last_chat_name = last_update['message']['chat']['first_name']
-
-        #if last_chat_text.lower() in greetings and today == now.day and 6 <= hour < 12:
-            #greet_bot.send_message(last_chat_id, 'Hi, {}'.format(last_chat_name))
-            #today += 1
-
-        #elif last_chat_text.lower() in greetings and today == now.day and 12 <= hour < 17:
-            #greet_bot.send_message(last_chat_id, 'Hi, {}'.format(last_chat_name))
-            #today += 1
-
-        #elif last_chat_text.lower() in greetings and today == now.day and 17 <= hour < 23:
-            #greet_bot.send_message(last_chat_id, 'Hi, {}'.format(last_chat_name))
-            #today += 1
-
-        #new_offset = last_update_id + 1
-
-if __name__ == '__main__':  
+if __name__ == '__main__':
+    signal.signal(signal.SIGTERM, signal_term_handler)
     try:
-        main()
+        set_hook = api.get(URL + "setWebhook?url=%s" % MyURL)
+        if set_hook.status_code != 200:
+            logging.error("Can't set hook: %s. Quit." % set_hook.text)
+            exit(1)
+        application.listen(8888)
+        tornado.ioloop.IOLoop.current().start()
     except KeyboardInterrupt:
-        exit()
+        signal_term_handler(signal.SIGTERM, None)
